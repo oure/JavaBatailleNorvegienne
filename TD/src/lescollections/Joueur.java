b369bccd9a7c4108d5bdaba6cabebbc1a745a3dc
GHCody <pa.bonnel@t.fr>
2014-11-16 20:00:25 +0100

    Initial set-up

---
+++ TD/src/lescollections/Joueur.java
@@ -0,0 +1,27 @@
+package lescollections;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Joueur {
+	String nom;
+	Set<Carte> set = new HashSet<Carte>();
+
+	public Joueur(String nom) {
+		this.nom = nom;
+	}
+
+	public void recevoirCarte(Carte carte) {
+		set.add(carte);
+	}
+
+	public String toString() {
+		String ss = null;
+		for (Iterator<Carte> iterator=set.iterator(); iterator.hasNext();) {
+			Carte carte = (Carte) iterator.next();
+			ss += carte.getValeur() + " ";
+		}
+		return "Nom : " + nom + ", Carte(s) : "+ss;
+	}
+}
---
+++ TD/src/lescollections/PartieDeCartes.java
@@ -0,0 +1,22 @@
+package lescollections;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class PartieDeCartes {
+	public static void main(String[] args) {
+		JeuDeCartes jeuDeCarte=new JeuDeCartes();
+		System.out.println("Jeu de 32 cartes : "+jeuDeCarte);
+		jeuDeCarte.melanger();
+		System.out.println("Jeu de 32 cartes melange : "+jeuDeCarte);
+		HashSet<Joueur> lj = new HashSet<Joueur>();
+		lj.add(new Joueur("Micky"));
+		lj.add(new Joueur("Johnny"));
+		lj.add(new Joueur("Joel"));
+		jeuDeCarte.distribuer(lj);
+		for (Iterator iterator = lj.iterator(); iterator.hasNext();) {
+			Joueur joueur = (Joueur) iterator.next();
+			System.out.println(joueur);
+		}
+	}
+}
