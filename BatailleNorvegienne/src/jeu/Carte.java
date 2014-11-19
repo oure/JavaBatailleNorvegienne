f198dc8489f22bcd7558ce76d4853bb73b0e33d6
GHCody <pa.bonnel@inventati.org>
2014-11-19 23:42:42 +0100

    Set up initial

---
+++ BatailleNorvegienne/src/jeu/Carte.java
@@ -0,0 +1,16 @@
+package jeu;
+
+public class Carte {
+	int valeur;
+	public Carte(int v){
+		valeur=v;
+	}
+	public int getValeur() {
+		return valeur;
+	}
+
+	public void setValeur(int valeur) {
+		this.valeur = valeur;
+	}
+	
+}
