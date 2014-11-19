bfcfec94444a5a8b6811d8c5586b4b28ec6d79ea
GHCody <pa.bonnel@inventati.org>
2014-11-19 23:28:32 +0100

    creation de la classe carte

---
+++ BatailleNorvegienne_LOO2/src/projet/Carte.java
@@ -0,0 +1,16 @@
+package projet;
+
+public class Carte {
+	int valeur;
+	public Carte(int v){
+		valeur=v;
+	}
+	public int getValeur() {
+		return valeur;
+	}
+
+	public void setValeur(int valeur) {
+		this.valeur = valeur;
+	}
+	
+}
