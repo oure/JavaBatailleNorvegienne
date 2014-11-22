76a27a6de4a7dabad035f4e9ee97543b68153d55
GHCody <pa.bonnel@inventati.org>
2014-11-22 16:51:07 +0100

    Modification de la classe partie ajout d'attribut

--- BatailleNorvegienne/src/jeu/PartieDeCartes.java
+++ BatailleNorvegienne/src/jeu/PartieDeCartes.java
@@ -2,9 +2,14 @@ package jeu;
 
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class PartieDeCartes {
-	public static void main(String[] args) {
+	Pioche pioche=new Pioche();
+	HashSet<JeuDeCartes> setJeuDeCartes=new HashSet<JeuDeCartes>();
+	LinkedList<Joueur> llJoueur=new LinkedList<Joueur>();
+
+		public static void main(String[] args) {
 		JeuDeCartes jeuDeCarte=new JeuDeCartes();
 		System.out.println("Jeu de 32 cartes : "+jeuDeCarte);
 		jeuDeCarte.melanger();
